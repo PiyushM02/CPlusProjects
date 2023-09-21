f6aefcd4bf9e5f97b7abcea5c94014eafb0d71b3
Piyush <78868303+PiyushM02@users.noreply.github.com>
2023-09-21 15:46:43 -0400

    initials first commit
    Program that prints out first two initials of my name.

---
+++ initials.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+  /* 
+        This is a program that prints out the first two initials of my name.
+  */
+ 
+  //Fun Fact: I love to play basketball.
+  cout << "PPPP    M   M"<< endl;
+  cout << "P    P  MM MM" << endl;
+  cout << "P    P  MM MM" << endl;
+  cout << "PPPP    M M M" << endl;
+  cout << "P       M   M" << endl;
+  cout << "P       M   M" << endl;
+  cout << "P       M   M" << endl;
+}
